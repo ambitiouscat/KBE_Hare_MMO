bccb96a200bf0bb1a7bcd65e70f33950272d1335
AmbitiousCat <1069023394@qq.com>
2019-10-11 17:04:29 +0800

    11

---
+++ Service/MyGameServerAssets/scripts/data/d_spaces.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+
+datas={1: {'entityType': 'Space', 'spawnPos': (0, 0, 0), 'resPath': 'spaces/xinshoucun', 'id': 1, 'type': 1, 'name': '新手村'}, 2: {'entityType': 'Space', 'spawnPos': (-97.9299, 0.0, -158.922), 'resPath': 'spaces/xinshoucun2', 'id': 2, 'type': 1, 'name': 'xinshoucun2'}}
+
+allDatas = {
+	'场景表':datas,
+}
